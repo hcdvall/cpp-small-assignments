c95b946b77048eb01a5cd48f32a014e83feb413b
Henrik <henrik.sedvall@gmail.com>
2022-03-04 17:25:54 +0100

    Start linear search word

---
+++ LinearSearchWord.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+void containsWord(string A, string B)
+{
+    char sentArray[A.length()];
+    for (int i = 0; i < A.length(); i++)
+    {
+        sentArray[i] = A[i];
+        cout << sentArray[i];
+    }
+
+    while ()
+    for (int i = 0; i < A.length(); i++)
+    {
+        if (sentArray[i] != B[i])
+    }
+
+}
+
+int main(int argc, char const *argv[])
+{
+    containsWord("Hej da", "da");
+    
+    return 0;
+}
